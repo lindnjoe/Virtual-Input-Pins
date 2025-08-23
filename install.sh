4ce122c1e92f46cfb187f9cd0b2688aee059a351
lindnjoe <32586000+lindnjoe@users.noreply.github.com>
2025-08-23 00:54:32 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,1 +1,38 @@
+#!/bin/bash
 
+KLIPPER_DIR="${HOME}/klipper"
+
+VIRTUAL_INPUT_PIN_DIR="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
+
+if [ ! -d "$KLIPPER_DIR" ]; then
+    echo "virtual_input_pin: klipper doesn't exist"
+    exit 1
+fi
+
+echo "virtual_input_pin: linking klippy to virtual_input_pin.py."
+
+if [ -e "${KLIPPER_DIR}/klippy/extras/virtual_input_pin.py" ]; then
+    rm "${KLIPPER_DIR}/klippy/extras/virtual_input_pin.py"
+fi
+ln -s "${VIRTUAL_INPUT_PIN_DIR}/virtual_input_pin.py" "${KLIPPER_DIR}/klippy/extras/virtual_input_pin.py"
+
+if ! grep -q "klippy/extras/virtual_input_pin.py" "${KLIPPER_DIR}/.git/info/exclude"; then
+    echo "klippy/extras/virtual_input_pin.py" >> "${KLIPPER_DIR}/.git/info/exclude"
+fi
+
+read -r -p "virtual_input_pin: install auto_ams_update.py? [y/N] " AUTO_AMS_CHOICE
+if [[ "${AUTO_AMS_CHOICE}" =~ ^[Yy]$ ]]; then
+    echo "virtual_input_pin: linking klippy to auto_ams_update.py."
+    if [ -e "${KLIPPER_DIR}/klippy/extras/auto_ams_update.py" ]; then
+        rm "${KLIPPER_DIR}/klippy/extras/auto_ams_update.py"
+    fi
+    ln -s "${VIRTUAL_INPUT_PIN_DIR}/auto_ams_update.py" "${KLIPPER_DIR}/klippy/extras/auto_ams_update.py"
+
+    if ! grep -q "klippy/extras/auto_ams_update.py" "${KLIPPER_DIR}/.git/info/exclude"; then
+        echo "klippy/extras/auto_ams_update.py" >> "${KLIPPER_DIR}/.git/info/exclude"
+    fi
+else
+    echo "virtual_input_pin: skipping auto_ams_update.py."
+fi
+
+echo "virtual_input_pin: installation successful."
